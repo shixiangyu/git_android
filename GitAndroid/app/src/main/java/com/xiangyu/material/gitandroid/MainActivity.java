6f34c3b0d1b44830b3807d4547793ec1eb91e967
史翔宇 <shixiangyu@inagora.cn>
2018-05-24 11:54:51 +0800

    添加git按钮

--- GitAndroid/app/src/main/java/com/xiangyu/material/gitandroid/MainActivity.java
+++ GitAndroid/app/src/main/java/com/xiangyu/material/gitandroid/MainActivity.java
@@ -2,6 +2,8 @@ package com.xiangyu.material.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.git).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("shixiangyu","git");
+            }
+        });
     }
 }
