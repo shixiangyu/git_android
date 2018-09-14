5dffadae8f695b465c4fd5da5304a16e1a50523f
shixiangyu <shixiangyu@inagora.cn>
2018-09-14 10:52:54 +0800

    TestBranchActivity增加onCreate方法

--- GitAndroid/app/src/main/java/com/xiangyu/material/gitandroid/TestBranchActivity.java
+++ GitAndroid/app/src/main/java/com/xiangyu/material/gitandroid/TestBranchActivity.java
@@ -1,7 +1,15 @@
 package com.xiangyu.material.gitandroid;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by shixiangyu on 2018/9/14.
  */
-public class TestBranchActivity {
+public class TestBranchActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
