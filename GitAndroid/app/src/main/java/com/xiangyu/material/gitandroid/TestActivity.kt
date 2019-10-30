772ee5667eb2c1d6b1174aaacabdb72002aa3366
shixiangyu <shixiangyu@inagora.cn>
2019-10-30 10:47:50 +0800

    TestActivity改为kotlin 编码

---
+++ GitAndroid/app/src/main/java/com/xiangyu/material/gitandroid/TestActivity.kt
@@ -0,0 +1,14 @@
+package com.xiangyu.material.gitandroid
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by shixiangyu on 2019-10-30.
+ */
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
